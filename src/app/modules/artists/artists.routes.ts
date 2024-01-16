e6e3fe22d1d550ea7a9c0906342139c6840bdb39
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-01-16 18:57:29 +0600

    Added atrist crud

--- src/app/modules/artists/artists.routes.ts
+++ src/app/modules/artists/artists.routes.ts
@@ -1,8 +1,27 @@
 import express from 'express';
 import { ArtistsController } from './artists.controller';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { ArtistsValidation } from './artists.validation';
 const router = express.Router();
 
 // Routes
-router.get('/', ArtistsController.getAllArtists);
+router.get('/', auth(), ArtistsController.getAllArtists);
+
+router.get('/:id', auth(), ArtistsController.getSingleArtist);
+
+router.patch(
+  '/',
+  auth(),
+  validateRequest(ArtistsValidation.updateArtistsJoiSchema),
+  ArtistsController.updateArtist
+);
+
+router.patch(
+  '/',
+  auth(),
+  validateRequest(ArtistsValidation.updateArtistsJoiSchema),
+  ArtistsController.updateArtist
+);
 
 export const ArtistsRoutes = router;
--- src/app/modules/artists/artists.service.ts
+++ src/app/modules/artists/artists.service.ts
@@ -1,4 +1,5 @@
 import DB from '../../../server';
+import { IArtists } from './artists.interface';
 
 const getAllArtists = async () => {
   const query = `Select * from artists`;
@@ -8,6 +9,33 @@ const getAllArtists = async () => {
   return result;
 };
 
+const getSingleArtist = async (id: string) => {
+  const query = `Select * from artists where id = ${id}`;
+  const result = DB.query(query).then(res => {
+    return res.rows;
+  });
+
+  return result;
+};
+
+const updateArtist = async (id: string, dataToUpdate: Partial<IArtists>) => {
+  const columns = Object.keys(dataToUpdate);
+  const values = Object.values(dataToUpdate);
+
+  const setClause = columns
+    .map((col, index) => `${col} = $${index + 1}`)
+    .join(', ');
+
+  const query = `UPDATE artists SET ${setClause} WHERE id = $${
+    columns.length + 1
+  } RETURNING *`;
+
+  const result = await DB.query(query, [...values, id]);
+  return result.rows;
+};
+
 export const ArtistsService = {
   getAllArtists,
+  getSingleArtist,
+  updateArtist,
 };

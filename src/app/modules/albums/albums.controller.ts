dd2a90bed8205b8a435fdc4105253dee8ae18f5e
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-01-17 09:57:10 +0600

    Added album get , update and delete api

--- src/app/modules/albums/albums.controller.ts
+++ src/app/modules/albums/albums.controller.ts
@@ -16,6 +16,55 @@ const createAlbum = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllAlbum = catchAsync(async (req: Request, res: Response) => {
+  const result = await AlbumService.getAllAlbum();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Albums fetched successfully!',
+    data: result,
+  });
+});
+
+const getSingleAlbum = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await AlbumService.getSingleAlbum(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Album fetched successfully!',
+    data: result,
+  });
+});
+
+const updateAlbum = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const dataToUpdate = req.body;
+  const result = await AlbumService.updateAlbum(id, dataToUpdate);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Album updated successfully!',
+    data: result,
+  });
+});
+
+const deleteAlbum = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await AlbumService.deleteAlbum(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Album deleted successfully!',
+    data: result,
+  });
+});
+
+
 export const AlbumsController = {
   createAlbum,
+  getAllAlbum,
+  getSingleAlbum,
+  updateAlbum,
+  deleteAlbum,
 };
